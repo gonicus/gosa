b4b5c9e6c37c7bd87fcd39b604d7fbe25c120f3e
Cajus Pollmeier <pollmeier@gonicus.de>
2012-07-11 13:20:09 +0200

    Really

--- source/class/cute/proxy/Object.js
+++ source/class/cute/proxy/Object.js
@@ -19,7 +19,7 @@ qx.Class.define("cute.proxy.Object", {
     // Add more translations to the qx.locale.Manager
     var lm = qx.locale.Manager.getInstance();
     console.log(this.translations);
-    lm.addTranslation(qx.locale.Manager.getLocale(), this.translations);
+    lm.addTranslation(qx.locale.Manager.getInstance().getLocale(), this.translations);
     
     // Initialization is done (Start sending attribute modifications to the backend)
     this.initialized = true;
