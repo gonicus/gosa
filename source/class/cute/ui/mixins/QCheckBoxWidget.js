7bd2d9a3d0d384870cc640fe04cd44de39576b52
Fabian Hickert <hickert@gonicus.de>
2012-07-23 11:13:39 +0000

    Updated checkbox widget

--- source/class/cute/ui/mixins/QCheckBoxWidget.js
+++ source/class/cute/ui/mixins/QCheckBoxWidget.js
@@ -9,6 +9,7 @@ qx.Mixin.define("cute.ui.mixins.QCheckBoxWidget",
       var widget = new cute.ui.widgets.QCheckBoxWidget();
       this.processCommonProperties(widget, props);
       this._widgets[name] = widget;
+      console.log(this._widgets);
 
       if("text" in props){
         widget.setLabel(props["text"]["string"]);
--- source/class/cute/ui/widgets/QCheckBoxWidget.js
+++ source/class/cute/ui/widgets/QCheckBoxWidget.js
@@ -9,6 +9,11 @@ qx.Class.define("cute.ui.widgets.QCheckBoxWidget", {
     this._chkBoxWidget = new qx.ui.form.CheckBox();
     this.add(this._chkBoxWidget);
     this.bind("label", this._chkBoxWidget, "label");
+    this._chkBoxWidget.addListener("changeValue", function(){
+        this.getValue().removeAll();
+        this.getValue().push(this._chkBoxWidget.getValue());
+        this.fireDataEvent("valueChanged", this.getValue());
+      }, this);
   },
 
   properties: {
@@ -26,77 +31,14 @@ qx.Class.define("cute.ui.widgets.QCheckBoxWidget", {
   members: {
 
     _chkBoxWidget: null,
-    _property_timer: null,
-    _was_manually_initialized: false,
-
-    _getCleanValue: function(){
-      this._chkBoxWidget.getValue();
-    },
-
-    /* Sends an update event for the current values of this widget.
-     * e.g. not empty.
-     * */
-    _updateValues: function(){
-      var ok = true;
-      for(var i=1; i< this._widgets.length; i++){
-        this._widgets[i].setValid(true);
-        if(this._widgets[i].getValue() == ""){
-          ok = false;
-          this._widgets[i].setValid(false);
-        }
-      }
-      if(ok){
-        this.setModified(true);
-        this.fireDataEvent("valueChanged", this._getCleanValue());
-      }
-    },
-
-    /* Creates an update-function for each widget to ensure that values are set
-     * after a given period of time.
-     */
-    __timedPropertyUpdater: function(id, userInput){
-      var func = function(value){
-        var timer = qx.util.TimerManager.getInstance();
-        this.addState("modified");
-        if(this._property_timer != null){
-          timer.stop(this._property_timer);
-          this._property_timer = null;
-        }
-        this._property_timer = timer.start(function(){
-          this.removeState("modified");
-          timer.stop(this._property_timer);
-          this._property_timer = null;
-          
-          this.getValue().setItem(id, userInput.getValue());
-          this._updateValues();
-        }, null, this, null, 2000);
-      }
-      return func;
-    },
-
-    /* This method returns a function which directly updates the property-value.
-     * */
-    __propertyUpdater: function(id, userInput){
-      var func = function(value){
-        var timer = qx.util.TimerManager.getInstance();
-        if(this._property_timer != null){
-          timer.stop(this._property_timer);
-          this._property_timer = null;
-        }
-        if(this.hasState("modified")){
-          this.removeState("modified");
-          this.getValue().setItem(id, userInput.getValue());
-          this._updateValues();
-        }
-      }
-      return func;
-    },
 
     /* Apply method for the value property.
      * This method will regenerate the gui.
      * */
     _applyValue: function(value, old_value){
-
+      if(value && value.length){
+        this._chkBoxWidget.setValue(value.getItem(0));
+      }
     },
 
     setInvalidMessage: function(message){
