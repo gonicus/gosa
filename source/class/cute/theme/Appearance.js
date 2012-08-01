46d6b73474750f2e4536d524348f7e2ffdd4195b
Fabian Hickert <hickert@gonicus.de>
2012-08-01 06:41:13 +0000

    Updated appearances

--- source/class/cute/theme/Appearance.js
+++ source/class/cute/theme/Appearance.js
@@ -14,21 +14,53 @@ qx.Theme.define("cute.theme.Appearance",
 
   appearances :
   {
-    "list" :
+    "SearchList" :
     {
       alias : "scrollarea"
       //,include : "textfield"
     },
 
-    "label" :
+    "SearchListItem-Icon" : 
+    {
+      style : function(states)
+      {
+        return {};
+      }
+    },
+
+    "SearchLIstItem-Dn" :
     {
       style : function(states)
       {
         return {
-          textColor : states.disabled ? "text-disabled" : "text"
+          textColor : "green",
+          cursor: "default"
         };
       }
     },
+
+    "SearchLIstItem-Title" :
+    {
+      style : function(states)
+      {
+        return {
+          textColor : "blue",
+          cursor: "pointer",
+          font : "SearchResultTitle"
+        };
+      }
+    },
+
+    "SearchLIstItem-Description" :
+    {
+      style : function(states)
+      {
+        return {
+          textColor : "black"
+        };
+      }
+    },
+
 
     "SearchListItem":
     {
