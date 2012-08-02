266c83d4ca665c0d5c02fc826201ad07d3817bb2
Cajus Pollmeier <pollmeier@gonicus.de>
2012-08-02 13:43:43 +0200

    Added new tabview/container

---
+++ source/class/cute/ui/container/SlideBar.js
@@ -0,0 +1,18 @@
+qx.Class.define("cute.ui.container.SlideBar",
+{
+  extend : qx.ui.container.SlideBar,
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  /**
+   * @param orientation {String?"horizontal"} The slide bar orientation
+   */
+  construct : function(orientation)
+  {
+    this.base(arguments);
+  }
+});
---
+++ source/class/cute/ui/tabview/TabView.js
@@ -0,0 +1,87 @@
+qx.Class.define("cute.ui.tabview.TabView",
+{
+  extend : qx.ui.tabview.TabView,
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+
+  /**
+   * @param barPosition {String} Initial bar position ({@link #barPosition})
+   */
+  construct : function(barPosition)
+  {
+    this.base(arguments);
+  },
+
+
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+
+  events :
+  {
+    /** Fires after the selection was modified */
+    //"changeSelection" : "qx.event.type.Data"
+  },
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+
+  properties :
+  {
+    // overridden
+    menu :
+    {
+      check : qx.ui.form.MenuButton,
+      nullable : true
+    }
+  },
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+
+  members :
+  {
+
+    // overridden
+    _createChildControlImpl : function(id, hash)
+    {
+      var control;
+
+      switch(id)
+      {
+        case "bar":
+          control = new qx.ui.container.SlideBar();
+          control.setZIndex(10);
+          this._add(control);
+          break;
+
+        case "pane":
+          control = new qx.ui.container.Stack;
+          control.setZIndex(5);
+          this._add(control, {flex:1});
+          break;
+      }
+
+      return control || this.base(arguments, id);
+    }
+  }
+});
