7f9e42f2273038483d945bd454190707b091d167
Fabian Hickert <hickert@gonicus.de>
2012-08-14 13:57:16 +0000

    Show group membership

--- source/class/cute/ui/widgets/QTableWidgetWidget.js
+++ source/class/cute/ui/widgets/QTableWidgetWidget.js
@@ -3,30 +3,46 @@ qx.Class.define("cute.ui.widgets.QTableWidgetWidget", {
   extend: cute.ui.widgets.Widget,
 
   construct: function(){
-  
+
     this.base(arguments);
-    this.setLayout(new qx.ui.layout.Canvas());
     this.setDecorator("main");
-    this._attributes = [];
+    this.setLayout(new qx.ui.layout.Canvas());
+    this._columNames = [];
+    this._tableData = [];
+    this.addListener("appear", function(){
+        this._createGui();
+      }, this);
   },
 
   members: {
 
-    _attributes: null,
     _table: null,
     _tableModel: null,
-  
-    _applyGuiProperties: function(props){
+    _tableData: null,
+    _columNames: null,
 
-      var columns = [];
-      for(var col in props['columns']){
-        columns.push(props['columns'][col]);
+    
+    _createGui: function(){
+      this._tableModel = new qx.ui.table.model.Simple(this._tableData);
+
+      this._tableModel.setColumns(this._columNames);
+
+      var data = [];
+      for(var i=0; i<this.getValue().getLength();i++){
+        this._tableData.push([this.getValue().getItem(i)]);
       }
-      this._tableModel = new qx.ui.table.model.Simple();
-      this._tableModel.setColumns(columns);
+      this._tableModel.setData(this._tableData);
       this._table = new qx.ui.table.Table(this._tableModel);
       this._table.setStatusBarVisible(false);
       this.add(this._table, {top:0 , bottom:0, right: 0, left:0});
+    },
+
+
+    _applyGuiProperties: function(props){
+      this._columNames = [];
+      for(var col in props['columns']){
+        this._columNames.push(props['columns'][col]);
+      }
     }
   }
 });
