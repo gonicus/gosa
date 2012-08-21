7054c0746566af0aceb03f830843dfcee27d9020
Cajus Pollmeier <pollmeier@gonicus.de>
2012-08-21 08:05:02 +0200

    Added more images

--- source/class/cute/ui/SearchListItem.js
+++ source/class/cute/ui/SearchListItem.js
@@ -22,8 +22,8 @@ qx.Class.define("cute.ui.SearchListItem", {
 
     // create and add Part 3 to the toolbar
     this._toolbar = new qx.ui.container.Composite(new qx.ui.layout.HBox(0));
-    var Button1 = new qx.ui.toolbar.Button(null, "cute/images/22/actions/dialog-ok.png");
-    var Button2 = new qx.ui.toolbar.Button(null, "cute/images/22/actions/dialog-cancel.png");
+    var Button1 = new qx.ui.toolbar.Button(null, "cute/images/22/actions/document-edit.png");
+    var Button2 = new qx.ui.toolbar.Button(null, "cute/images/22/actions/document-close.png");
     this._toolbar.add(Button1);
     this._toolbar.add(Button2);
     this._toolbar.setAllowGrowY(false);
