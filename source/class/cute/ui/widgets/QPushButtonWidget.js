0a78531923c54acfb1a3c01e5c3a0cee111d1a3f
Cajus Pollmeier <pollmeier@gonicus.de>
2012-08-31 16:59:04 +0200

    Added button

--- source/class/cute/ui/widgets/QPushButtonWidget.js
+++ source/class/cute/ui/widgets/QPushButtonWidget.js
@@ -17,18 +17,19 @@
 /**
  * This is the main application class of your custom application "cute"
  */
-qx.Class.define("cute.ui.widgets.LineWidget",
+qx.Class.define("cute.ui.widgets.QPushButtonWidget",
 {
   extend : cute.ui.widgets.Widget,
 
   properties: {
 
-    orientation : {
-      init : "Qt::Horizontal",
-      check : ["Qt::Horizontal", "Qt::Vertical"],
-      apply : "_setOrientation",
+    text : {
+      init : "",
+      check : "String",
+      apply : "_setText",
       nullable: true
     }
+
   },
 
   /*
@@ -42,38 +43,27 @@ qx.Class.define("cute.ui.widgets.LineWidget",
 
     // Call super class
     this.setLayout(new qx.ui.layout.Canvas());
-    this._widget = new qx.ui.core.Widget();
+    this._widget = new qx.ui.form.Button();
     this.add(this._widget, {top:0, bottom:0, left:0, right: 0});
-
-    this._setOrientation("Qt::Horizontal");
   },
 
   members :
   {
     _widget: null,
 
-    _setOrientation : function(value)
+    _setText : function(value)
     {
-      if("Qt::Horizontal") {
-        this._widget.setDecorator("separator-vertical");
-        this._widget.setMarginTop(10);
-        this._widget.setHeight(8);
-        this._widget.resetWidth();
-      } else {
-        this._widget.setDecorator("separator-horizontal");
-        this._widget.setMarginLeft(10);
-        this._widget.resetHeight();
-        this._widget.setWidth(8);
-      }
+      this._widget.setLabel(this.tr(value));
     },
 
     /* Apply collected gui properties to this widet
      * */
     _applyGuiProperties: function(props){
-      if(props["orientation"] && props["orientation"]["enum"]){
-        this._setOrientation(props["orientation"]["enum"]);
+      if(props["text"] && props["text"]["string"]){
+        this._setText(props["text"]["string"]);
       }
     }
+
   }
 });
 
