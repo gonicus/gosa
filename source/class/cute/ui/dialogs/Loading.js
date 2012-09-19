804c033d4b00584ef2c295198c7002750e4cd079
Fabian Hickert <hickert@gonicus.de>
2012-09-19 09:46:20 +0000

    cleanup

--- source/class/cute/ui/dialogs/Loading.js
+++ source/class/cute/ui/dialogs/Loading.js
@@ -5,19 +5,12 @@ qx.Class.define("cute.ui.dialogs.Loading",
   construct : function()
   {
     this.base(arguments, this.tr("Loading"));
-    this.label = new qx.ui.basic.Label();
+    this.label = new qx.ui.basic.Label("");
     this.add(this.label);
-    this.setLabel("");
-  },
-
-  events: {
-    "login": "qx.event.type.Event"
   },
 
   members: {
-
     label: null,
-
     setLabel: function(action){
       this.label.setValue(action);
     }
