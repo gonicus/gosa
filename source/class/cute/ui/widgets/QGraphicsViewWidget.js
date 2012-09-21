155f1171b5c3a5a32215d11efa87a9afd2718031
Cajus Pollmeier <pollmeier@gonicus.de>
2012-09-21 10:31:14 +0200

    Added sample capture

--- source/class/cute/ui/widgets/QGraphicsViewWidget.js
+++ source/class/cute/ui/widgets/QGraphicsViewWidget.js
@@ -24,10 +24,47 @@ qx.Class.define("cute.ui.widgets.QGraphicsViewWidget", {
     var container = new qx.ui.container.Composite(new qx.ui.layout.Canvas());
     container.add(this._widget, {top:1, bottom:1, left:1, right:1});
     this.add(container);
+
+    // Pre-create a video capture area
+    this.__cap = new capture.Capture().set({
+        width: 320,
+        height: 240
+    });
+    this.__cap_win = new qx.ui.window.Window(this.tr("Video capture"));
+    this.__cap_win.setShowMaximize(false);
+    this.__cap_win.setShowMinimize(false);
+    this.__cap_win.setAlwaysOnTop(true);
+    this.__cap_win.setLayout(new qx.ui.layout.VBox(10));
+    this.__cap_win.add(this.__cap);
+    this.__cap_win.addListener("beforeClose", function() {
+        this.__cap.stop();
+    }, this);
+
+    // Add capture button
+    var paneLayout = new qx.ui.layout.HBox().set({
+      spacing: 4,
+      alignX : "center"
+    });
+    var buttonPane = new qx.ui.container.Composite(paneLayout);
+    var cap_button = new qx.ui.form.Button(null,
+            cute.Config.getImagePath("actions/attribute-capture.png", 22));
+    cap_button.setWidth(64);
+    cap_button.addListener('execute', function() {
+        this.setValue(new qx.data.Array([this.__cap.getImageData('jpeg', 80, 0, 480, 480, 0, 0, 200, 200)]));
+        this.fireDataEvent("changeValue", this.getValue());
+        this.__cap.stop();
+        this.__cap_win.hide();
+    }, this);
+
+    buttonPane.add(cap_button);
+    this.__cap_win.add(buttonPane); 
     
     // Create context menu buttons
     this._changePicture = new qx.ui.menu.Button(this.tr("Upload new image"),
             cute.Config.getImagePath("actions/attribute-choose.png", 22));
+    this._capturePicture = new qx.ui.menu.Button(this.tr("Capture"),
+            cute.Config.getImagePath("actions/attribute-capture.png", 22));
+    this._capturePicture.setEnabled(this.__cap.isSupported());
     this._removePicture = new qx.ui.menu.Button(this.tr("Remove image"),
             cute.Config.getImagePath("actions/attribute-remove.png", 22));
     this._removePicture.setEnabled(false);
@@ -55,9 +92,21 @@ qx.Class.define("cute.ui.widgets.QGraphicsViewWidget", {
         this.fireDataEvent("changeValue", this.getValue());
       }, this);
 
+    // Create capture image handler
+    this._capturePicture.addListener("click", function(){
+        //this.setValue(new qx.data.Array());
+        //this.fireDataEvent("changeValue", this.getValue());
+
+        // Open Capture window and start capturing
+        this.__cap_win.show();
+        this.__cap_win.center();
+        this.__cap.start();
+      }, this);
+
     // Create context menu
     var menu = new qx.ui.menu.Menu();
     menu.add(this._changePicture);
+    menu.add(this._capturePicture);
     menu.add(this._removePicture);
     container.setContextMenu(menu);
     container.addListener('click', function(e){
@@ -84,13 +133,16 @@ qx.Class.define("cute.ui.widgets.QGraphicsViewWidget", {
     /* Apply method for the value property.
      * */
     _applyValue: function(value, old_value){
-      if(this._widget){
+      if (this._widget){
         this._removePicture.setEnabled(false);
-        if(value && value.length && value.getItem(0)){
-          var source = "data:image/jpeg;base64," + value.getItem(0).get();
-          this._widget.setSource(source);
-          this._removePicture.setEnabled(true);
-        }else{
+        if (value && value.length && value.getItem(0)){
+            var source = value.getItem(0);
+            if (typeof(source) != 'string') {
+              source = "data:image/jpeg;base64," + source.get();
+            }
+            this._widget.setSource(source);
+            this._removePicture.setEnabled(true);
+        } else{
           this._widget.setSource(this._defaultImage);
         }
       }
