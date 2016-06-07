36c59ca3d7a84101f4852007eae7dd43e1a8cc41
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-06-07 10:00:59 +0200

    hard coded path for gui

--- plugins/gui/src/gosa/plugin/gui/main.py
+++ plugins/gui/src/gosa/plugin/gui/main.py
@@ -1,11 +1,14 @@
 import pkg_resources
+import os
 from flask import send_from_directory
 from flask.views import MethodView
 
 class GuiPlugin(MethodView):
 
     def __init__(self):
-        self.root = pkg_resources.resource_filename('gosa.plugin.gui', 'frontend/build')
+        #self.root = pkg_resources.resource_filename('gosa.plugin.gui', 'frontend/build')
+        # TODO: hardcoded path should be replaced later
+        self.root = os.path.join(os.getcwd(), 'plugins', 'gui', 'frontend', 'gosa', 'build')
 
     def get(self, path):
         return send_from_directory(self.root, path)
