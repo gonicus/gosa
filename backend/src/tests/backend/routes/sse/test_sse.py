779f96428f80099e242c8f7b473886a10b9747a3
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-06-20 09:47:28 +0200

    add some callbacks to stabilize the tests

--- backend/src/tests/backend/routes/sse/test_sse.py
+++ backend/src/tests/backend/routes/sse/test_sse.py
@@ -32,31 +32,32 @@ class SseHandlerTestCase(AsyncHTTPTestCase):
         self.stop()
 
     def test_subscribe(self):
-        self.http_client.fetch(self.get_url('/events'), streaming_callback=self.handleMessage)
+        self.http_client.fetch(self.get_url('/events'), self.stop, streaming_callback=self.handleMessage)
         # post something
         self.check_data = "Test"
-        self.http_client.fetch(self.get_url('/events'), method="POST", body=self.check_data)
+        self.http_client.fetch(self.get_url('/events'), self.stop, method="POST", body=self.check_data)
         self.wait()
 
     def test_missed_events(self):
         # initial connection
-        future = self.http_client.fetch(self.get_url('/events'), streaming_callback=self.handleMessage)
+        future = self.http_client.fetch(self.get_url('/events'), self.stop, streaming_callback=self.handleMessage)
         self.check_data = "Test"
-        self.http_client.fetch(self.get_url('/events'), method="POST", body=self.check_data)
+        self.http_client.fetch(self.get_url('/events'), self.stop, method="POST", body=self.check_data)
         self.wait()
         del future
 
         self.check_data = "Deferred Test"
-        self.http_client.fetch(self.get_url('/events'), method="POST", body=self.check_data)
+        self.http_client.fetch(self.get_url('/events'), self.stop, method="POST", body=self.check_data)
+        self.wait()
 
-        self.http_client.fetch(self.get_url('/events'),
+        self.http_client.fetch(self.get_url('/events'), self.stop,
                                headers={'Last-Event-ID': self.last_id})
         self.wait()
 
     def test_named_events(self):
-        self.http_client.fetch(self.get_url('/events'), streaming_callback=self.handleMessage)
+        self.http_client.fetch(self.get_url('/events'), self.stop, streaming_callback=self.handleMessage)
 
         self.check_data = "Test"
         self.check_event = "txt"
-        self.http_client.fetch(self.get_url('/events?event=txt'), method="POST", body=self.check_data)
+        self.http_client.fetch(self.get_url('/events?event=txt'), self.stop, method="POST", body=self.check_data)
         self.wait()
