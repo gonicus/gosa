fb1fbd45a3c24618ea9e8ed3a443bc43fb3e3951
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-07-07 09:42:07 +0200

    keep + use the exit codes from the submodule runs

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ modules = [
   'backend',
   'shell'
 ]
-
+return_code = 0
 for module in modules:
-    os.system("cd %s && ./setup.py %s" % (module, " ".join(sys.argv[1:])))
+    return_code = max(return_code, os.system("cd %s && ./setup.py %s" % (module, " ".join(sys.argv[1:]))) >> 8)
 
 for root, dirs, files in os.walk("plugins"):
     if "setup.py" in files:
-        os.system("cd %s && ./setup.py %s" % (root, " ".join(sys.argv[1:])))
+        return_code = max(return_code, os.system("cd %s && ./setup.py %s" % (root, " ".join(sys.argv[1:]))) >> 8)
+
+sys.exit(return_code)
