a6e6f9c5ef7bea7e1aefc93388758e08dc7d63ee
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-07-12 11:18:48 +0200

    fix sqllite in-memory db initialization, generate index immediately during unittest and delayed by scheduler otherwise

--- conftest.py
+++ conftest.py
@@ -1,5 +1,15 @@
 import pytest
+import sys
+
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
+
+
+def pytest_configure(config):
+    sys._called_from_test = True
+
+
+def pytest_unconfigure(config):
+    del sys._called_from_test
