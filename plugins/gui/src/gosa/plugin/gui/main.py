4c59e97a53dbec884e08d76f34965ef7c0b5ede6
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-07-14 07:48:12 +0200

    normalize paths

--- plugins/gui/src/gosa/plugin/gui/main.py
+++ plugins/gui/src/gosa/plugin/gui/main.py
@@ -12,10 +12,10 @@ class GuiPlugin(tornado.web.StaticFileHandler):
         default = "index.html"
 
         if env.config.get("gui.debug", "false") == "true":  # pragma: nocover
-            path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                '..', '..', '..', '..', 'frontend')
+            path = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    '..', '..', '..', '..', 'frontend'))
             default = "gosa/source/index.html"
         else:
-            path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                '..', '..', '..', '..', 'frontend', 'gosa', 'build')
+            path = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                    '..', '..', '..', '..', 'frontend', 'gosa', 'build'))
         super(GuiPlugin, self).initialize(path, default)
