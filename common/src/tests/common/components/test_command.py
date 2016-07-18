594165fd4583a2f905569558729c603e0c42d5e3
Joshua <joshua.muehlfort@gonicus.de>
2016-07-18 14:10:42 +0200

    Further tests for command.py Test generation of __doc__ partly based on __help__

--- common/src/tests/common/components/test_command.py
+++ common/src/tests/common/components/test_command.py
@@ -3,12 +3,43 @@
 import unittest
 from gosa.common.components.command import *
 
+def add_help():
+    def decorate(f):
+        setattr(f, "__help__", "Help string")
+        return f
+    return decorate
+
 class CommandTestCase(unittest.TestCase):
-    @Command(__help__="TEST")
     def test_command(self):
-        pass
+        @Command()
+        @add_help()
+        def test():
+            """Docstring\nInfo"""
+            pass
+        assert test.__doc__ == """.. command:: backend test
+
+    Help string
+
+.. note::
+    **This method will be exported by the CommandRegistry.**
+
+Docstring
+Info"""
     
-    @Command()
     def test_command2(self):
-        """Docs"""
-        pass
+        @Command()
+        def test():
+            """Docstring\nInfo"""
+            pass
+        assert test.__doc__ == """.. command:: client test
+
+    
+Docstring
+Info
+
+..  note::
+    **This method will be exported by the CommandRegistry.**
+
+Docstring
+Info"""
+        
