ce2ce9a37304feec157445684a8ec93cf0d7b193
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-07-26 12:02:11 +0200

    cleanup

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import sys
 from gosa.common import Environment
-from gosa.common.components import PluginRegistry, ObjectRegistry
 import os
 
 def pytest_addoption(parser):
@@ -15,15 +14,10 @@ def pytest_configure(config):
 
 def pytest_unconfigure(config):
     del sys._called_from_test
-    # PluginRegistry.getInstance('HTTPService').srv.stop()
-    # PluginRegistry.shutdown()
 
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.conf")
+    Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_conf")
     Environment.noargs = True
-    #
-    # oreg = ObjectRegistry.getInstance()  # @UnusedVariable
-    # pr = PluginRegistry()  # @UnusedVariable
-    # cr = PluginRegistry.getInstance("CommandRegistry") # @UnusedVariable
+
