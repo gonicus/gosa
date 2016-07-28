fd38105efeeef3881fd3a5ec3e26aac5db547616
Joshua <joshua.muehlfort@gonicus.de>
2016-07-28 13:45:31 +0200

    Services Plugin: Check for "active" and "running"

--- dbus/src/gosa/dbus/plugins/services/main.py
+++ dbus/src/gosa/dbus/plugins/services/main.py
@@ -152,7 +152,8 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
         for unit in units:
             if not unit[0].endswith(".service"):
                 continue
-            services[unit[0]] = {"active": ["True" if unit[3] == "active" else "False"]}
+            services[unit[0]] = {"active": ["True" if unit[3] == "active" else "False"],
+                    "running": ["True" if unit[4] == "running" else "False"]}
         return services
 
     @dbus.service.method('org.gosa')
