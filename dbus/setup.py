3e35fc1363a7b47ee29dd1c67d18e0f49017e47f
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-08-23 15:27:01 +0200

    move dependencies around

--- dbus/setup.py
+++ dbus/setup.py
@@ -58,7 +58,8 @@ setup(
         'python_dateutil',
         'pytest-runner',
         'dbus-python',
-        'pyinotify'
+        'pyinotify',
+        'gi'
         ],
 
     install_requires = [
