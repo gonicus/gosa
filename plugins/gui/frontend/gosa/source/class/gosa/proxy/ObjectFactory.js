46e5092d035960c6acc016da69e4bd363f61dc70
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-11-23 11:02:31 +0100

    refactor class generation

--- plugins/gui/frontend/gosa/source/class/gosa/proxy/ObjectFactory.js
+++ plugins/gui/frontend/gosa/source/class/gosa/proxy/ObjectFactory.js
@@ -74,68 +74,8 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
           var baseType = null;
           var extensionTypes = null;
           var extensionDeps = null;
-          var attribute_data = {};
 
           var locale = gosa.Config.getLocale();
-
-          // This method is called below to make the code more readable.
-          var _handleResult = function(){
-
-            // This is the new classname for the metaclass.
-            // e.g. objects.User or workflows.create_user
-            var className = object_type + "s." + baseType;
-
-            // The base member variables for the metaclass
-            var members = {
-              instance_uuid: null,
-              dn: null,
-              methods: methods,
-              attributes: attributes,
-              attribute_data: attribute_data,
-              baseType: baseType,
-              extensionTypes: extensionTypes,
-              extensionDeps: extensionDeps,
-              locale: locale
-            };
-
-            // this closure returns a new apply method for the given attribute.
-            var getApplyMethod = function(name){
-              var func = function(value){
-                this.setAttribute(name, value);
-              };
-              return(func);
-            };
-
-            // this closure returns a new wrapper-method for an object method
-            var getMethod = function(name){
-              var func = function(){
-                return(this.callMethod.apply(this, [name].concat(Array.prototype.slice.call(arguments))));
-              };
-              return(func);
-            };
-
-            // Create list of properties
-            var properties = {};
-            for(var attr in attributes){
-              var name = attributes[attr];
-              var upperName = name.charAt(0).toUpperCase() + name.slice(1);
-              var applyName = "_apply_" + upperName;
-              var prop = {apply: applyName, event: "changed" + upperName, nullable: true, check: "qx.data.Array"};
-              members[applyName] = getApplyMethod(name);
-              properties[name] = prop;
-            }
-
-            // Create methods
-            for(attr in methods){
-              var name = methods[attr];
-              members[name] = getMethod(name);
-            }
-
-            // Create meta class for this object
-            var def = {extend: gosa.proxy.Object, members: members, properties: properties};
-            gosa.proxy.ObjectFactory.classes[className] = qx.Class.define(className, def);
-            c_callback.apply(c_context, [new gosa.proxy.ObjectFactory.classes[className](userData)]);
-          };
 
           if (object_type === "object") {
             // Load object info - base type, extension types
@@ -154,8 +94,8 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
                   }
                   else {
                     // Call the result handling method, we had defined earlier above.
-                    attribute_data = _attribute_data;
-                    _handleResult(userData);
+                    var className = this.__createClass(object_type, data, methods, attributes, _attribute_data, locale);
+                    c_callback.apply(c_context, [new gosa.proxy.ObjectFactory.classes[className](userData)]);
                   }
                 }, this, "dispatchObjectMethod", uuid, "get_attributes", true);
 
@@ -168,8 +108,14 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
                 c_callback.apply(c_context, [null, error]);
               }
               else {
-                // Call the result handling method, we had defined earlier above.
-                var className = this.__createWorkflowClass(uuid, methods, attributes, _attribute_data, locale);
+                var data = {
+                  className : "workflows."+uuid,
+                  id : uuid,
+                  base : "Workflow."+uuid,
+                  extensions: null,
+                  extensionDeps: null
+                };
+                var className = this.__createClass(object_type, data, methods, attributes, _attribute_data, locale);
                 c_callback.apply(c_context, [new gosa.proxy.ObjectFactory.classes[className](userData)]);
               }
             }, this, "dispatchObjectMethod", uuid, "get_attributes", true);
@@ -178,22 +124,25 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
       }, this, "openObject", object_type, dn, type);
     },
 
-    __createWorkflowClass : function(id, methods, attributes, attribute_data, locale) {
+    __createClass: function(object_type, data, methods, attributes, attribute_data, locale) {
       // This is the new classname for the metaclass.
-      // e.g. workflows.create_user
-      var className = "workflows." + id;
+      // e.g. objects.User or workflows.create_user
+      var className = data['className'] ? data['className'] : object_type+"s."+data['base'];
 
-      // The base member variables for the metaclass
       var members = {
         instance_uuid: null,
         dn: null,
-        id: id,
         methods: methods,
         attributes: attributes,
         attribute_data: attribute_data,
-        baseType: "Workflow."+id,
+        baseType: data['base'],
+        extensionTypes: data['extensions'],
+        extensionDeps: data['extension_deps'],
         locale: locale
       };
+      if (data['id']) {
+        members['id'] = data['id'];
+      }
 
       // this closure returns a new apply method for the given attribute.
       var getApplyMethod = function(name){
@@ -215,7 +164,7 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
       var properties = {};
       for(var attr in attributes){
         var name = attributes[attr];
-        var upperName = name.charAt(0).toUpperCase() + name.slice(1);
+        var upperName = qx.lang.String.firstUp(name);
         var applyName = "_apply_" + upperName;
         var prop = {apply: applyName, event: "changed" + upperName, nullable: true, check: "qx.data.Array"};
         members[applyName] = getApplyMethod(name);
