e9d10e2fdaf5be8b86722218165e71e0d3ce450b
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-11-25 16:42:50 +0100

    bind to this

--- plugins/gui/frontend/gosa/source/class/gosa/proxy/ObjectFactory.js
+++ plugins/gui/frontend/gosa/source/class/gosa/proxy/ObjectFactory.js
@@ -85,7 +85,7 @@ qx.Class.define("gosa.proxy.ObjectFactory", {
           });
 
         } else if (object_type === "workflow") {
-          return rpc.cA("dispatchObjectMethod", uuid, "get_attributes", true)
+          return rpc.cA("dispatchObjectMethod", uuid, "get_attributes", true).bind(this)
           .then(function(_attribute_data) {
 
             var data = {
