7dd7e3bb74fa84630b6e017fa0ec776deaebc20a
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2016-11-28 15:03:20 +0100

    fix the unrejected promis error

--- plugins/gui/frontend/gosa/source/class/gosa/ui/form/WindowListItem.js
+++ plugins/gui/frontend/gosa/source/class/gosa/ui/form/WindowListItem.js
@@ -22,7 +22,6 @@ qx.Class.define("gosa.ui.form.WindowListItem", {
 
     object: {
       check: "gosa.proxy.Object",
-      async: true,
       nullable: true,
       apply: "_applyObject"
     }
@@ -36,19 +35,22 @@ qx.Class.define("gosa.ui.form.WindowListItem", {
   members : {
     _applyObject: function(object) {
       if (object) {
-        // try to get the search result for this dn, to get the mapped title/icon values
-        return gosa.io.Rpc.getInstance().cA("getObjectSearchItem", object.dn)
-        .then(function(result) {
-          this.setLabel(result.title);
-          this.setIcon(result.icon ? result.icon : gosa.util.Icons.getIconByType(result.tag, 22));
-        }, this)
-        .catch(function(error) {
-          this.error(error);
-          // fallback
-          var dnPart = qx.util.StringSplit.split(qx.util.StringSplit.split(object.dn, "\,")[0], "=")[1];
-          this.setLabel(dnPart);
-        }, this);
-
+        // we need to break out of the property apply chain to allow promises to be used
+        // otherwise we get a warning about a created but not returned promise
+        new qx.util.DeferredCall(function() {
+          // try to get the search result for this dn, to get the mapped title/icon values
+          gosa.io.Rpc.getInstance().cA("getObjectSearchItem", object.dn)
+          .then(function(result) {
+            this.setLabel(result.title);
+            this.setIcon(result.icon ? result.icon : gosa.util.Icons.getIconByType(result.tag, 22));
+          }, this)
+          .catch(function(error) {
+            this.error(error);
+            // fallback
+            var dnPart = qx.util.StringSplit.split(qx.util.StringSplit.split(object.dn, "\,")[0], "=")[1];
+            this.setLabel(dnPart);
+          }, this);
+        }, this).schedule();
       }
     }
   }
