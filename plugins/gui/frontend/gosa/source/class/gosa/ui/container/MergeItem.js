c5b4dccb83dcc46a0363038b24d122d6a4fc7787
Markus Bader <bader@gonicus.de>
2017-01-11 15:02:26 +0100

    Show and use merge widgets on backend changes

--- plugins/gui/frontend/gosa/source/class/gosa/ui/container/MergeItem.js
+++ plugins/gui/frontend/gosa/source/class/gosa/ui/container/MergeItem.js
@@ -14,10 +14,28 @@
 
 qx.Class.define("gosa.ui.container.MergeItem", {
 	extend: qx.ui.form.ToggleButton,
+
 	construct: function(widget){
 		this.base(arguments);
 		this._setLayout(new qx.ui.layout.HBox());
+		this.__widget = widget;
 		this._add(widget, {flex: 1});
-        this.setDecorator("white-box");
-	}
+    this.setDecorator("white-box");
+	},
+
+  members : {
+
+	  __widget : null,
+
+    /**
+     * @return {var} The value of the inner widget (i.e. the first one)
+     */
+	  getInnerValue : function() {
+      return this.__widget.getChildren()[0].getValue();
+    }
+  },
+
+  destruct : function() {
+	  this._disposeObjects("__widget");
+  }
 });
--- plugins/gui/frontend/gosa/source/class/gosa/ui/dialogs/MergeDialog.js
+++ plugins/gui/frontend/gosa/source/class/gosa/ui/dialogs/MergeDialog.js
@@ -11,20 +11,21 @@
    See the LICENSE file in the project's top-level directory for details.
 
 ======================================================================== */
-
-/*
-#asset(gosa/*)
-*/
+/**
+ * Dialog window that can show several widgets to merge the value of an attribute.
+ */
 qx.Class.define("gosa.ui.dialogs.MergeDialog", {
 
   extend: gosa.ui.dialogs.Dialog,
 
-  construct: function(mods, exts, relations, ext_dependencies, order)
-  {
+  construct: function(mods, exts, relations, ext_dependencies, order) {
     this.base(arguments, this.tr("Merge required"), gosa.Config.getImagePath("status/dialog-information.png", 22));
-    this.setResizable(true);
-    this.setWidth(800);
-    this.setHeight(500);
+    this.set({
+      resizable : true,
+      width : 800,
+      height : 500,
+      autoDispose : true
+    });
 
     var layout = new qx.ui.layout.Grid(15);
     layout.setColumnFlex(1, 1);
@@ -33,19 +34,19 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
     var changesPane = new qx.ui.container.Composite(layout);
 
     var row = 0;
-    var itemid = 0;
-    var callonce = [];
-    var extlist = {};
+    var itemId = 0;
+    var callOnce = [];
+    var extList = {};
     var selections = [];
     var extensions = [];
     var items = {};
     var that = this;
-    var firstAttr = true
-    var firstExt = true
+    var firstAttr = true;
+    var firstExt = true;
 
     // This method adds a new merge-selector for a property.
-    var createSelector = function(name, desc, choice1, choice2){
-      if(firstAttr){
+    var createSelector = function(name, desc, choice1, choice2) {
+      if (firstAttr) {
         row ++;
         layout.setRowHeight(row, 20);
         row ++;
@@ -55,7 +56,7 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
         changesPane.add(new qx.ui.basic.Label(that.tr("Server")).set({font: 'bold'}), {row:row, column:2});
       }
       row ++;
-      itemid ++;
+      itemId ++;
       layout.setRowFlex(row);
 
       // Create the toggle fields
@@ -69,7 +70,11 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
       group.add(right);
 
       // Add the elements to the grid layout
-      changesPane.add(new qx.ui.basic.Label(that["tr"](desc)).set({rich: true}), {row: row, column: 0});  // jshint ignore:line
+      var translateString = desc;
+      if (typeof translateString === "string") {
+        translateString = that["tr"](desc);
+      }
+      changesPane.add(new qx.ui.basic.Label(translateString).set({rich: true}), {row: row, column: 0});  // jshint ignore:line
       changesPane.add(left, {row: row, column: 1});
       changesPane.add(right, {row: row, column: 2});
 
@@ -77,14 +82,14 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
       selections.push(function(){
         return({name: name, choice: left.getValue() && left.isEnabled()});
       });
-    }
+    };
 
     // This method creates an extension merge selector.
-    var createExtension = function(name, enabled){
+    var createExtension = function(name, enabled) {
       var desc = name;
 
       // Add header
-      if(firstExt){
+      if (firstExt) {
         row ++;
         layout.setRowHeight(row, 20);
         row ++;
@@ -96,27 +101,28 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
 
       // Prepare the layout
       row ++;
-      itemid ++;
+      itemId ++;
       layout.setRowFlex(row);
 
       // Create the selector
       var group = new qx.ui.form.RadioGroup();
       var left = null;
       var right = null;
-      if(enabled){
+      if (enabled) {
         left = new gosa.ui.container.MergeItem(new qx.ui.basic.Label("disabled"));
         right = new gosa.ui.container.MergeItem(new qx.ui.basic.Label("enabled"));
-      }else{
+      }
+      else {
         left = new gosa.ui.container.MergeItem(new qx.ui.basic.Label("enabled"));
         right = new gosa.ui.container.MergeItem(new qx.ui.basic.Label("disabled"));
       }
       left.setAppearance("mergeButton");
       right.setAppearance("mergeButton");
-      extlist[name] = [left, right];
+      extList[name] = [left, right];
       group.add(left);
       group.add(right);
 
-      if(relations['buddyTexts'][desc]){
+      if (relations['buddyTexts'][desc]) {
         desc = relations['buddyTexts'][desc];
       }
 
@@ -124,71 +130,73 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
       changesPane.add(new qx.ui.basic.Label(that["tr"](desc)).set({rich: true}), {row: row, column: 0});  // jshint ignore:line
       changesPane.add(left, {row: row, column: 1});
       changesPane.add(right, {row: row, column: 2});
-      extensions.push(function(){
+      extensions.push(function() {
         return({name: name, choice: left.getValue() && left.isEnabled()});
       });
 
-      // Act on selection changes of the extension selectors
-      // (disable property selectors and those extension that do not
-      // fullfil their requirements)
-      var func = function(e){
+      // Act on selection changes of the extension selectors (disable property selectors and those extension that do not
+      // fulfil their requirements)
+      var func = function() {
+        var tmp, state;
 
         // Disable all extension according to their dependencies
-        for(var ename in ext_dependencies){
-          for(var tmp in ext_dependencies[ename]){
-            if(ext_dependencies[ename][tmp] == name){
-              if(ename in extlist){
-                var state = !enabled && left.getValue() || enabled && right.getValue();
-                extlist[ename][0].setEnabled(state);
-                extlist[ename][1].setEnabled(state);
+        for (var ename in ext_dependencies) {
+          for (tmp in ext_dependencies[ename]) {
+            if (ext_dependencies[ename][tmp] === name) {
+              if (ename in extList) {
+                state = !enabled && left.getValue() || enabled && right.getValue();
+                extList[ename][0].setEnabled(state);
+                extList[ename][1].setEnabled(state);
               }
             }
           }
         }
 
         // Enable/disable property switches
-        var state = left.getValue();
-        if(enabled){
+        state = left.getValue();
+        if (enabled) {
           state = !state;
         }
-        if(relations['widgets'][name]){
-          for(var citem in relations['widgets'][name]){
-            var tmp = relations['widgets'][name][citem];
-            if(relations['bindings'][tmp]){
+        if (relations['widgets'][name]) {
+          for (var citem in relations['widgets'][name]) {
+            tmp = relations['widgets'][name][citem];
+            if (relations['bindings'][tmp]) {
               var item = relations['bindings'][tmp];
-              if(items[item]){
-                for(var i in items[item]){
+              if (items[item]) {
+                for (var i in items[item]) {
                   items[item][i].setEnabled(state);
                 }
               }
             }
           }
         }
-      }
+      };
 
-      // Disable extension dependen selectors
+      // Disable extension dependent selectors
       left.addListener("changeValue", func, this);
-      callonce.push(func);
-    }
+      callOnce.push(func);
+    };
+
+    var id;
 
     // Add extension actions in correct order
-    for(var id in order){
-      if(qx.lang.Array.contains(exts['added'], order[id])){
+    for (id in order) {
+      if (qx.lang.Array.contains(exts['added'], order[id])) {
         createExtension(order[id], true);
       }
-      if(qx.lang.Array.contains(exts['removed'], order[id])){
+      if (qx.lang.Array.contains(exts['removed'], order[id])) {
         createExtension(order[id], false);
       }
     }
 
     // Add the collected attrs
-    for(var id in mods){
-      createSelector(mods[id]['name'], mods[id]['desc'], mods[id]['value_1'], mods[id]['value_2']);
-    }
+    mods.forEach(function(item) {
+      createSelector(item.attributeName, item.label, item.localWidget, item.remoteWidget);
+    });
 
     // Ensure that the selectors are valid.
-    for(var fid in callonce){
-      callonce[fid]();
+    for (var fid in callOnce) {
+      callOnce[fid]();
     }
 
     // Add Elements into a scroll area
@@ -200,18 +208,21 @@ qx.Class.define("gosa.ui.dialogs.MergeDialog", {
     this.addButton(ok);
 
     // When OK is pressed then send an event containing the user decissions.
-    ok.addListener("execute", function(){
+    ok.addListener("execute", function() {
         var res = {};
         var ext = {};
-        for(var item in selections){
-          var tmp = selections[item]();
+        var item, tmp;
+
+        for (item in selections) {
+          tmp = selections[item]();
           res[tmp['name']] = tmp['choice'];
         }
-        for(var item in extensions){
-          var tmp = extensions[item]();
+        for (item in extensions) {
+          tmp = extensions[item]();
           ext[tmp['name']] = tmp['choice'];
         }
-        this.fireDataEvent("merge", {'attrs': res, 'ext' : ext});
+        this.fireDataEvent("merge", {'attributes': res, 'ext' : ext});
+        this.close();
       }, this);
   },
 
