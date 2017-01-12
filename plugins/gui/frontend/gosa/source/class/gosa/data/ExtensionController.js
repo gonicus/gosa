3ca6e042e23b2e541c0c0734cc09d89dd5417df8
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-01-12 10:31:54 +0100

    fix obj access

--- plugins/gui/frontend/gosa/source/class/gosa/data/ExtensionController.js
+++ plugins/gui/frontend/gosa/source/class/gosa/data/ExtensionController.js
@@ -180,8 +180,8 @@ qx.Class.define("gosa.data.ExtensionController", {
       return this._obj.extend(extension)
       .then(function () {
         return qx.Promise.all([
-          this.__obj.refreshMetaInformation(),
-          this.__obj.refreshAttributeInformation()
+          this._obj.refreshMetaInformation(),
+          this._obj.refreshAttributeInformation()
         ]);
       }, this)
       .then(function() {
@@ -214,8 +214,8 @@ qx.Class.define("gosa.data.ExtensionController", {
       return this._obj.retract(extension)
       .then(function() {
         return qx.Promise.all([
-          this.__obj.refreshMetaInformation(),
-          this.__obj.refreshAttributeInformation()
+          this._obj.refreshMetaInformation(),
+          this._obj.refreshAttributeInformation()
         ]);
       }, this)
       .then(function() {
