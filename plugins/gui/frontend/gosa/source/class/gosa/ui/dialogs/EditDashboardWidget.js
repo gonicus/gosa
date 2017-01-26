aec8557580b62437829342bf519af736641d7054
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-01-26 08:01:09 +0100

    check for modification

--- plugins/gui/frontend/gosa/source/class/gosa/ui/dialogs/EditDashboardWidget.js
+++ plugins/gui/frontend/gosa/source/class/gosa/ui/dialogs/EditDashboardWidget.js
@@ -21,21 +21,20 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
   construct: function(widget) {
     this.base(arguments, this.tr("Edit dashboard widget"));
 
+    this.__selectionValues = {};
+    this.__initialValues = {};
+
     // form
-    var form = new qx.ui.form.Form();
-
+    var form = this.__form = new qx.ui.form.Form();
     // add the form items
-    form.add(new qx.ui.form.TextField(widget.getBackgroundColor()), this.tr("Background color"), null, "backgroundColor");
-
-    var properties = [];
-    var selectionValues = {};
+    this.addFormItem(new qx.ui.form.TextField(widget.getBackgroundColor()), this.tr("Background color"), null, "backgroundColor");
 
     var options = gosa.data.DashboardController.getWidgetOptions(widget);
     if (options.settings) {
       Object.getOwnPropertyNames(options.settings.properties).forEach(function(propertyName) {
         var typeSettings = options.settings.properties[propertyName];
         var type, title = propertyName;
-        properties.push(propertyName);
+        
         if (qx.lang.Type.isObject(typeSettings)) {
           type = typeSettings.type;
           if (typeSettings.title) {
@@ -48,23 +47,23 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
         } else {
           type = typeSettings;
         }
-        var formItem;
+        var formItem, value = widget.get(propertyName);
+        this.__initialValues[propertyName] = value === null ? "" : value;
         switch (type) {
           case "Json":
-            formItem = new qx.ui.form.TextArea(qx.lang.Json.stringify(widget.get(propertyName)));
+            formItem = new qx.ui.form.TextArea(qx.lang.Json.stringify(value));
             break;
           case "String":
-            formItem = new qx.ui.form.TextArea(widget.get(propertyName));
+            formItem = new qx.ui.form.TextArea(value);
             break;
           case "Number":
-            formItem = new qx.ui.form.Spinner(widget.get(propertyName));
+            formItem = new qx.ui.form.Spinner(value);
             break;
           case "selection":
             var selectBox = new qx.ui.form.SelectBox();
             if (!options.settings.mandatory || options.settings.mandatory.indexOf(propertyName) === -1) {
               selectBox.add(new qx.ui.form.ListItem("-"));
             }
-            var currentValue = widget.get(propertyName);
 
             if (typeSettings.provider === "RPC" && typeSettings.method) {
               // retrieve data from rpc
@@ -78,7 +77,7 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
                       item.setIcon(result[key][typeSettings.icon])
                     }
                     selectBox.add(item);
-                    if (keyValue === currentValue) {
+                    if (keyValue === value) {
                       selectBox.setSelection([item]);
                     }
                   }
@@ -89,7 +88,7 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
               if (selectBox.getSelection().length) {
                 var selectedValue = selectBox.getSelection()[0].getUserData("key");
                 if (selectedValue) {
-                  selectionValues[propertyName] = selectedValue;
+                  this.__selectionValues[propertyName] = selectedValue;
                 }
               }
             }, this);
@@ -99,13 +98,14 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
         if (options.settings.mandatory && options.settings.mandatory.indexOf(propertyName) >= 0) {
           formItem.setRequired(true);
         }
-        form.add(formItem, title, null, propertyName);
+        this.addFormItem(formItem, title, null, propertyName);
       }, this);
     }
 
     // buttons
     var saveButton = gosa.ui.base.Buttons.getOkButton();
     saveButton.setAppearance("button-primary");
+    saveButton.setEnabled(false);
     this.addButton(saveButton);
     var cancelButton = gosa.ui.base.Buttons.getCancelButton();
     this.addButton(cancelButton);
@@ -114,27 +114,36 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
     this.addElement(new gosa.ui.form.renderer.Single(form));
 
     var controller = new qx.data.controller.Form(null, form);
-    var model = controller.createModel();
+    this.__model = controller.createModel();
+    this.bind("modified", saveButton, "enabled");
 
     // serialization and reset /////////
     saveButton.addListener("execute", function() {
       if (form.validate()) {
-        if (model.getBackgroundColor()) {
-          widget.setBackgroundColor(model.getBackgroundColor());
-        } else {
-          widget.resetBackgroundColor();
+        if (this.isModified()) {
+          Object.getOwnPropertyNames(this.__initialValues).forEach(function(prop) {
+            var value = this.__selectionValues[prop] || this.__model.get(prop);
+            widget.set(prop, value);
+          }, this);
+          this.fireEvent("modified");
         }
-        properties.forEach(function(prop) {
-          var value = selectionValues[prop] || model.get(prop);
-          widget.set(prop, value);
-        });
-        this.fireEvent("modified");
         this.close();
       }
     }, this);
     cancelButton.addListener("execute", function() {
-      form.reset();
-      this.close();
+      if (this.isModified()) {
+        var dialog = new gosa.ui.dialogs.Confirmation(this.tr("Unsaved changes"), this.tr("Do you want to discard those changes?"), "warning");
+        dialog.addListenerOnce("confirmed", function(ev) {
+          if (ev.getData() === true) {
+            form.reset();
+            this.close();
+          }
+        }, this);
+        dialog.open();
+      } else {
+        form.reset();
+        this.close();
+      }
     }, this);
 
   },
@@ -146,6 +155,69 @@ qx.Class.define("gosa.ui.dialogs.EditDashboardWidget", {
   */
   events : {
     "modified": "qx.event.type.Event"
+  },
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+  properties : {
+    modified: {
+      check: "Boolean",
+      init: false,
+      event: "changeModified"
+    }
+  },
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+  members : {
+    __initialValues: null,
+    __selectionValues: null,
+    __model: null,
+    __form: null,
+    
+    addFormItem: function(item, label, validator, name) {
+      item.setUserData("property", name);
+      this.__form.add(item, label, validator, name);
+      if (item instanceof qx.ui.form.SelectBox) {
+        item.addListener("changeSelection", this.checkModification, this);
+      } else {
+        item.setLiveUpdate(true);
+        item.addListener("changeValue", this.checkModification, this);
+      }
+    },
+
+    /**
+     * Check form for modifications
+     * @return {Boolean} true if something has been modified
+     */
+    checkModification: function(ev) {
+      var item = ev.getTarget();
+      var prop = item.getUserData("property");
+      var value = null;
+      if (item instanceof qx.ui.form.SelectBox) {
+        if (item.getSelection().length) {
+          value = item.getSelection()[0].getUserData("key");
+        }
+      } else {
+        value = item.getValue();
+      }
+      this.setModified(value != this.__initialValues[prop]);
+    }
+  },
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+  destruct : function() {
+    this._disposeObjects("__initialValues", "__selectionValues", "__model", "__form");
   }
 
 });
