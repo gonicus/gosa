1e4b6bc5e0b4a2518b892ab5ff976e8bf3a901ef
Markus Bader <baderm@gonicus.de>
2017-02-02 09:48:28 +0100

    Refactored QTableWidgetWidge

--- plugins/gui/frontend/gosa/source/class/gosa/ui/widgets/QTableWidgetWidget.js
+++ plugins/gui/frontend/gosa/source/class/gosa/ui/widgets/QTableWidgetWidget.js
@@ -14,7 +14,7 @@
 
 /* This class is a qooxdoo-widget representation of the cure-QtableWidget.
  *
- * Notice that this plugin can occure in two different forms.
+ * Notice that this plugin can occur in two different forms.
  *
  * 1. The first form is a table like widget as used on the PosixUser-tab
  *    It allows to select multiple values for a single property.
@@ -28,55 +28,23 @@ qx.Class.define("gosa.ui.widgets.QTableWidgetWidget", {
 
   extend: gosa.ui.widgets.Widget,
 
-  construct: function(){
+  construct: function() {
     this.base(arguments);
     this.contents.setLayout(new qx.ui.layout.Canvas());
-    var attrs = ["buddyOf","attribute",
-          "labelText","extension","guiProperties","caseSensitive",
-          "blockedBy","defaultValue","dependsOn","mandatory",
-          "type","unique","values","readOnly","multivalue",
-          "initComplete", "enabled",
-          "value","required","placeholder","maxLength","modified"];
+    this.contents.add(new qx.ui.core.Spacer(1), {edge: 0});
 
-    this.contents.add(new qx.ui.core.Spacer(1), {left:0, right:0, bottom: 0, top:0});
-
-    this.addListenerOnce("appear", function() {
-      // Create the multi-select style widget or the single select widget depending on the source-properties multivalue
-      // state.
-
-      if (this.isMultivalue()) {
-        this._widget = new gosa.ui.widgets.TableWithSelector();
-        this._createChildControl("add-button");
-        this._createChildControl("remove-button");
-        this._widget.bind("hasSelection", this.getChildControl("remove-button"), "enabled");
-      } else {
-        this._widget = new gosa.ui.widgets.SingleSelector();
-      }
-
-      this._widget.addListener("changeValue", function(e){
-        this.fireDataEvent("changeValue", e.getData());
-      }, this);
-
-      for (var attr in attrs){
-        this.bind(attrs[attr], this._widget, attrs[attr]);
-      }
-
-      this.contents.add(this._widget, {left:0, right:0, bottom: 37, top:0});
-    }, this);
+    this.addListenerOnce("appear", this.__createWidget, this);
   },
 
   statics: {
-
-    /* Create a readonly representation of this widget for the given value.
-     * This is used while merging object properties.
-     * */
     getMergeWidget: function(value){
       var container = new qx.ui.container.Composite(new qx.ui.layout.VBox());
-      for(var i=0;i<value.getLength(); i++){
-        var w = new qx.ui.form.TextField(value.getItem(i));
-        w.setReadOnly(true);
-        container.add(w);
-      }
+
+      value.forEach(function(item) {
+        var widget = new qx.ui.form.TextField(item);
+        widget.setReadOnly(true);
+        container.add(widget);
+      });
       return(container);
     }
   },
@@ -90,20 +58,53 @@ qx.Class.define("gosa.ui.widgets.QTableWidgetWidget", {
 
   members: {
 
-    _widget: null,
+    __widget: null,
 
-    /* Sets an error message for the widget given by id.
-     */
     setErrorMessage: function(message, id){
-      this._widget.setErrorMessage(message, id);
+      this.__widget.setErrorMessage(message, id);
     },
 
-    /* Resets error messages
-     * */
     resetErrorMessage: function(){
-      if(this._widget){
-        this._widget.resetErrorMessage();
+      if (this.__widget) {
+        this.__widget.resetErrorMessage();
       }
+    },
+
+    __createWidget : function() {
+      if (this.isMultivalue()) {
+        this.__createMultivalueWidget();
+      }
+      else {
+        this.__createSingleValueWidget();
+      }
+      this.__widget.addListener("changeValue", this.__onWidgetChangeValue, this);
+      this.__bindAttributesToWidget();
+      this.contents.add(this.__widget, {left: 0, right: 0, bottom: 37, top: 0});
+    },
+
+    __createSingleValueWidget : function() {
+      this.__widget = new gosa.ui.widgets.SingleSelector();
+    },
+
+    __createMultivalueWidget : function() {
+      this.__widget = new gosa.ui.widgets.TableWithSelector();
+      this._createChildControl("add-button");
+      this._createChildControl("remove-button");
+      this.__widget.bind("hasSelection", this.getChildControl("remove-button"), "enabled");
+    },
+
+    __onWidgetChangeValue : function(event) {
+      this.fireDataEvent("changeValue", event.getData());
+    },
+
+    __bindAttributesToWidget: function() {
+      var attrs = ["buddyOf", "attribute", "labelText", "extension", "guiProperties", "caseSensitive", "blockedBy",
+        "defaultValue", "dependsOn", "mandatory", "type", "unique", "values", "readOnly", "multivalue", "initComplete",
+        "enabled", "value", "required", "placeholder", "maxLength", "modified"];
+
+      attrs.forEach(function(attributeName) {
+        this.bind(attributeName, this.__widget, attributeName);
+      }, this);
     },
 
     _createChildControlImpl : function(id, hash) {
@@ -118,13 +119,13 @@ qx.Class.define("gosa.ui.widgets.QTableWidgetWidget", {
 
         case "add-button":
           control = new qx.ui.form.Button(this.tr("Add"), "@Ligature/edit/22");
-          control.addListener("execute", this._widget.openSelector, this._widget);
+          control.addListener("execute", this.__widget.openSelector, this.__widget);
           this.getChildControl("control-bar").add(control);
           break;
 
         case "remove-button":
           control = new qx.ui.form.Button(this.tr("Remove"), "@Ligature/trash/22");
-          control.addListener("execute", this._widget.removeSelection, this._widget);
+          control.addListener("execute", this.__widget.removeSelection, this.__widget);
           this.getChildControl("control-bar").add(control);
           break;
       }
@@ -138,6 +139,6 @@ qx.Class.define("gosa.ui.widgets.QTableWidgetWidget", {
     // Remove all listeners and then set our values to null.
     qx.event.Registration.removeAllListeners(this);
 
-    this._disposeObjects("_widget");
+    this._disposeObjects("__widget");
   }
 });
