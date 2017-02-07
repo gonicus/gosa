cacbcf0f42d0705e070abd44c777a84780649264
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-02-07 11:34:35 +0100

    fix ldap monitor tests

--- utils/src/tests/utils/test_gosa_ldap_monitor.py
+++ utils/src/tests/utils/test_gosa_ldap_monitor.py
@@ -8,14 +8,14 @@
 # See the LICENSE file in the project's top-level directory for details.
 from base64 import b64encode
 from unittest import mock, TestCase
-from datetime import datetime
+
 from gosa.utils.gosa_ldap_monitor import *
 
 
 class LdapMonitorTestCase(TestCase):
 
     def test_main(self):
-        handler = mock.MagicMock()
+
         lines = [
             "dn:base",
             "changetype:modify",
@@ -23,21 +23,25 @@ class LdapMonitorTestCase(TestCase):
             "modifyTimestamp:%s" % datetime.now().strftime("%Y%m%d%H%M%SZ"),
             ""
         ]
+        env = Environment.getInstance()
+        webhook_target = env.config.get('backend-monitor.webhook-target', default='http://localhost:8000/hooks/')
+        token = bytes(env.config.get('backend-monitor.webhook-token'), 'ascii')
         m_open = mock.mock_open()
         with mock.patch("gosa.utils.gosa_ldap_monitor.open", m_open, create=True),\
-                mock.patch("gosa.utils.gosa_ldap_monitor.tail", return_value=lines) as m_tail:
-            monitor('/tmp/audit.log', 'other', handler)
-            assert not handler.send_event.called
+                mock.patch("gosa.utils.gosa_ldap_monitor.tail", return_value=lines) as m_tail,\
+                mock.patch("gosa.utils.gosa_ldap_monitor.requests.post") as m_post:
+            monitor('/tmp/audit.log', 'other', token, webhook_target)
+            assert not m_post.called
 
-            monitor('/tmp/audit.log', 'gosa-backend', handler)
-            assert handler.send_event.called
+            monitor('/tmp/audit.log', 'gosa-backend', token, webhook_target)
+            assert m_post.called
 
-            handler.reset_mock()
+            m_post.reset_mock()
             m_tail.return_value = [
                 "dn::%s" % b64encode(b"base"),
                 "changetype:modify",
                 "modifiersName:other",
                 ""
             ]
-            monitor('/tmp/audit.log', 'gosa-backend', handler)
-            assert handler.send_event.called
+            monitor('/tmp/audit.log', 'gosa-backend', token, webhook_target)
+            assert m_post.called
