24fb827edb67b66977df49f8c2719826b3199518
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-02-17 10:16:42 +0100

    use the correct handler in GUI test

--- plugins/gui/frontend/gosa/source/class/gosa/test/data/SettingsRegistryTest.js
+++ plugins/gui/frontend/gosa/source/class/gosa/test/data/SettingsRegistryTest.js
@@ -22,7 +22,7 @@ qx.Class.define("gosa.test.data.SettingsRegistryTest", {
   members : {
 
     testSetData : function() {
-      gosa.data.SettingsRegistry.registerHandler(new gosa.data.settings.ConfigHandler("gosa.settings"));
+      gosa.data.SettingsRegistry.registerHandler(new gosa.data.settings.Handler("gosa.settings"));
 
       // mock RPCs
       this.sinon.stub(gosa.io.Rpc.getInstance(), "cA");
