74b6b8f9f572085dcc8584af847648435f973dc2
Markus Bader <baderm@gonicus.de>
2017-03-01 14:13:08 +0100

    Fixed unit tests for generateUid

--- backend/src/tests/backend/plugins/user/test_main.py
+++ backend/src/tests/backend/plugins/user/test_main.py
@@ -7,8 +7,9 @@
 #
 # See the LICENSE file in the project's top-level directory for details.
 
-from unittest import TestCase
+from unittest import TestCase, mock
 from gosa.backend.plugins.user.main import User
+from gosa.common import Environment
 
 
 class UserMainTestCase(TestCase):
@@ -17,42 +18,61 @@ class UserMainTestCase(TestCase):
         user = TestUser()
         assert user is not None
 
+        env = Environment.getInstance().config
+
         # enable test when using gosa's transliterate
-        assert user.generateUid('{%sn}', {'sn': 'SmÜrübröd'}) == ['smueruebroed']
-        assert user.generateUid('{%prename} {%sn}', {'prename': 'Владимир', 'sn': 'Путин'}) == ['vladimir putin']
+        with mock.patch.object(env, 'get') as mock_config:
+            mock_config.return_value = '{%sn}'
+            assert user.generateUid({'sn': 'SmÜrübröd'}) == ['SmUeruebroed']
 
-        assert user.generateUid('{%sn}', {'sn': 'Doe'}) == ['doe']
-        assert user.generateUid('{%sn}', {'sn': 'Dorian'}) == ['dorian']
-        assert user.generateUid('{%sn}-{%prename}', {'sn': 'Doe', 'prename': 'John'}) == ['doe-john']
+            mock_config.return_value = '{%prename} {%sn}'
+            assert user.generateUid({'prename': 'Владимир', 'sn': 'Путин'}) == ['Vladimir Putin']
 
-        assert user.generateUid('{%sn[1]}', {'sn': 'Doe'}) == ['o']
-        assert user.generateUid('{%sn[1-3]}', {'sn': 'Dorian'}) == ['o', 'or']
-        assert user.generateUid('{%prename}-{%sn[1-3]}', {'sn': 'Dorian', 'prename': 'John'}) == ['john-o', 'john-or']
-        assert user.generateUid('{%prename[1-3]}-{%sn[1-3]}', {'sn': 'Dorian', 'prename': 'John'}) == ['o-o', 'o-or', 'oh-o', 'oh-or']
+            mock_config.return_value = '{%sn}'
+            assert user.generateUid({'sn': 'Doe'}) == ['Doe']
+            assert user.generateUid({'sn': 'Dorian'}) == ['Dorian']
+            mock_config.return_value = '{%sn}-{%prename}'
+            assert user.generateUid({'sn': 'Doe', 'prename': 'John'}) == ['Doe-John']
 
-        assert user.generateUid('{%Sn}', {'sn': 'Doe'}) == ['doe']
+            mock_config.return_value = '{%sn[1]}'
+            assert user.generateUid({'sn': 'Doe'}) == ['o']
+            mock_config.return_value = '{%sn[1-3]}'
+            assert user.generateUid({'sn': 'Dorian'}) == ['o', 'or']
+            mock_config.return_value = '{%prename}-{%sn[1-3]}'
+            assert user.generateUid({'sn': 'Dorian', 'prename': 'John'}) == ['John-o', 'John-or']
+            mock_config.return_value = '{%prename[1-3]}-{%sn[1-3]}'
+            assert user.generateUid({'sn': 'Dorian', 'prename': 'John'}) == ['o-o', 'o-or', 'oh-o', 'oh-or']
 
-        assert user.generateUid('{%sn}-{id:3}', {'sn': 'Doe'}) == ['doe-000']
-        user.existing_uids = ['doe-000', 'doe-001', 'doe-002']
-        assert user.generateUid('{%sn}-{id:3}', {'sn': 'Doe'}) == ['doe-003']
-        assert user.generateUid('{%sn[0-3]}-{id:3}', {'sn': 'Doe'}) == ['d-000', 'do-000', 'doe-003']
+            mock_config.return_value = '{%Sn}'
+            assert user.generateUid({'Sn': 'Doe'}) == ['Doe']
 
-        random_id = user.generateUid('{id#3}', None)[0]
-        assert len(random_id) == 3
-        assert int(random_id) >= 100
-        assert int(random_id) < 1000
+            mock_config.return_value = '{%sn}-{id:3}'
+            assert user.generateUid({'sn': 'doe'}) == ['doe-000']
+            user.existing_uids = ['doe-000', 'doe-001', 'doe-002']
+            mock_config.return_value = '{%sn}-{id:3}'
+            assert user.generateUid({'sn': 'doe'}) == ['doe-003']
+            mock_config.return_value = '{%sn[0-3]}-{id:3}'
+            assert user.generateUid({'sn': 'doe'}) == ['d-000', 'do-000', 'doe-003']
 
-        user.existing_uids = []
-        assert user.generateUid('{%sn}{id!2}', {'sn': 'Doe'}) == ['doe']
-        user.existing_uids = ['doe']
-        assert user.generateUid('{%sn}{id!2}', {'sn': 'Doe'}) == ['doe01']
-        user.existing_uids = ['doe', 'doe01']
-        assert user.generateUid('{%sn}{id!2}', {'sn': 'Doe'}) == ['doe02']
-        user.existing_uids = ['doe01']
-        assert user.generateUid('{%sn}{id!2}', {'sn': 'Doe'}) == ['doe']
+            mock_config.return_value = '{id#3}'
+            random_id = user.generateUid(None)[0]
+            assert len(random_id) == 3
+            assert int(random_id) >= 100
+            assert int(random_id) < 1000
 
-        user.existing_uids = ['doe']
-        assert user.generateUid('{%sn}', {'sn': 'Doe'}) == []
+            mock_config.return_value = '{%sn}{id!2}'
+            user.existing_uids = []
+            assert user.generateUid({'sn': 'doe'}) == ['doe']
+            user.existing_uids = ['doe']
+            assert user.generateUid({'sn': 'doe'}) == ['doe01']
+            user.existing_uids = ['doe', 'doe01']
+            assert user.generateUid({'sn': 'doe'}) == ['doe02']
+            user.existing_uids = ['doe01']
+            assert user.generateUid({'sn': 'doe'}) == ['doe']
+
+            mock_config.return_value = '{%sn}'
+            user.existing_uids = ['doe']
+            assert user.generateUid({'sn': 'doe'}) == []
 
 
 class TestUser(User):
