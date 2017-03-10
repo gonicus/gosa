3ac98d81059d3dcab31d074d89d5b10393e934f7
Markus Bader <baderm@gonicus.de>
2017-03-10 10:02:23 +0100

    Updated workflow

--- example/workflows/create_user/workflow.py
+++ example/workflows/create_user/workflow.py
@@ -1,10 +1,30 @@
-given_name = data["givenName"].lower()
-surname = data["sn"].lower()
+# example for 'data'
+# {
+#   'mail': ['foo@bar.com', 'bar@foo.com'],
+#   'sn': 'Doe',
+#   'cn': None,
+#   'homePhone': '123',
+#   'parentDn': 'dc=example,dc=net',
+#   'uid': 'Doe-J',
+#   'givenName': 'John'
+# }
 
-test = dict(cn="%s %s" % (data["givenName"], data["sn"]), uid="%s_%s" % (given_name, surname), mail="%s.%s@eyjafjallajökull.is" % (given_name, surname))
+# extract target location
+parent_dn = data['parentDn']
+del data['parentDn']
 
-print("---------------------------------------------")
-print(data)
-print(test)
-print(getWorkflows())
-print("---------------------------------------------")
+# create object
+obj = openObject('object', parent_dn, 'User')
+ref_uuid = obj['__jsonclass__'][1][1]
+print(1)
+dispatchObjectMethod(ref_uuid, 'extend', 'ZarafaAccount')
+print(2)
+
+data['zarafaResourceType'] = "0"
+
+for attr_name, value in data.items():
+    print('setting', attr_name, value)
+    setObjectProperty(ref_uuid, attr_name, value)
+
+print(3)
+dispatchObjectMethod(ref_uuid, 'commit')
