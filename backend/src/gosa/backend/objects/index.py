6e8c2389ea91b8cba1c871684197552a8ccc6b79
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-10-04 14:57:37 +0200

    use left join for extension queries
    closes #121

--- backend/src/gosa/backend/objects/index.py
+++ backend/src/gosa/backend/objects/index.py
@@ -868,12 +868,7 @@ class ObjectIndex(Plugin):
         # Add query information to be able to search various tables
         _args = __make_filter(node)
 
-        if use_extension:
-            args = [ObjectInfoIndex.uuid == ExtensionIndex.uuid]
-            args += _args
-            return and_(*args)
-
-        return _args
+        return _args, use_extension
 
     def search(self, query, properties):
         """
@@ -891,7 +886,7 @@ class ObjectIndex(Plugin):
         ``Return``: List of dicts
         """
         res = []
-        fltr = self._make_filter(query)
+        fltr, use_extension = self._make_filter(query)
 
         def normalize(data, resultset=None):
             _res = {
@@ -923,7 +918,10 @@ class ObjectIndex(Plugin):
 
             return _res
 
-        q = self.__session.query(ObjectInfoIndex).filter(*fltr)
+        if use_extension is True:
+            q = self.__session.query(ObjectInfoIndex).outerjoin(ObjectInfoIndex.extensions).filter(*fltr)
+        else:
+            q = self.__session.query(ObjectInfoIndex).filter(*fltr)
 
         self.log.debug(str(q.statement.compile(dialect=postgresql.dialect(), compile_kwargs={"literal_binds": True})))
 
