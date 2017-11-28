3a8945aeebd48db05f685344d7e2462714c4f7d8
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 15:08:15 +0100

    use config option

--- backend/conftest.py
+++ backend/conftest.py
@@ -8,12 +8,12 @@
 # See the LICENSE file in the project's top-level directory for details.
 
 import pytest
-import psutil
 from gosa.backend.main import *
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
+    parser.addoption("--travis", action="store_true", default=False, help="Use travis config for tests")
 
 
 def pytest_configure(config):
@@ -28,14 +28,7 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    travis = False
-    for pid in psutil.pids():
-        environ = psutil.Process(pid).environ()
-        if 'TRAVIS' in environ:
-            travis = True
-            break
-
-    if travis:
+    if pytest.config.getoption("--travis"):
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "travis_conf")
     else:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "test_conf")
