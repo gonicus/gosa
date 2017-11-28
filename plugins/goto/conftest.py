ddce802d653100263076735025719bd9deaed708
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 11:58:12 +0100

    set own env var

--- plugins/goto/conftest.py
+++ plugins/goto/conftest.py
@@ -16,7 +16,7 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    if os.environ.get("TRAVIS_BRANCH") is not None:
+    if os.environ.get("TRAVIS") is not None:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "travis_conf")
     else:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "test_conf")
