76d4689e3d8c5f5e798e4c3857b297387fad4f38
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 16:06:09 +0100

    check option

--- backend/conftest.py
+++ backend/conftest.py
@@ -29,7 +29,8 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    if pytest.config.getoption("--travis"):
+    print("Travis set %s" % pytest.config.getoption("--travis"))
+    if pytest.config.getoption("--travis") is True:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "travis_conf")
     else:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "test_conf")
