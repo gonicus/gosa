3f8ad0675f187acb88bbe2742fe289920b6be46e
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 15:36:06 +0100

    change the global config

--- conftest.py
+++ conftest.py
@@ -3,9 +3,10 @@ import sys
 from gosa.common import Environment
 import os
 
+
 def pytest_addoption(parser):
-    parser.addoption("--runslow", action="store_true",
-                     help="run slow tests")
+    parser.addoption("--runslow", action="store_true", help="run slow tests")
+    parser.addoption("--travis", action="store_true", default=False, help="Use travis config for tests")
 
 
 def pytest_configure(config):
@@ -15,9 +16,14 @@ def pytest_configure(config):
 def pytest_unconfigure(config):
     del sys._called_from_test
 
+
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_conf")
+    Environment.reset()
+    if pytest.config.getoption("--travis"):
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "travis_conf")
+    else:
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "test_conf")
     Environment.noargs = True
 
