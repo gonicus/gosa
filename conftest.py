bec5b807410ae350db6737fda8f8ca7c5674152f
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 15:44:29 +0100

    fix path

--- conftest.py
+++ conftest.py
@@ -20,10 +20,9 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    Environment.reset()
     if pytest.config.getoption("--travis"):
-        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "travis_conf")
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "travis_conf")
     else:
-        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "test_conf")
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_conf")
     Environment.noargs = True
 
