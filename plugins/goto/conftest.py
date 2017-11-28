e7168f11adaff4cfd56240d01f6572d5c0ebd387
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-28 11:45:23 +0100

    add special travis test-config

--- plugins/goto/conftest.py
+++ plugins/goto/conftest.py
@@ -16,7 +16,10 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "test_conf")
+    if os.environ.get("TRAVIS_BRANCH") is not None:
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "travis_conf")
+    else:
+        Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "test_conf")
     Environment.noargs = True
 
     oreg = ObjectRegistry.getInstance()  # @UnusedVariable
