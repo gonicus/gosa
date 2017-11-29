63cf993e6517c68c41e92e0b2ad480baa00d6c4c
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2017-11-29 08:41:42 +0100

    remove dbus-launch + cleanup

--- backend/conftest.py
+++ backend/conftest.py
@@ -29,18 +29,11 @@ def pytest_unconfigure(config):
 @pytest.fixture(scope="session", autouse=True)
 def use_test_config():
     Environment.reset()
-    print("Travis set %s" % pytest.config.getoption("--travis"))
     if pytest.config.getoption("--travis") is True:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "travis_conf")
     else:
         Environment.config = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "test_conf")
     Environment.noargs = True
 
-    env = Environment.getInstance()
-    workflow_path = env.config.get("core.workflow_path", "/var/lib/gosa/workflows")
-
-    # create workflow path
-    if not os.path.exists(workflow_path):
-        os.makedirs(workflow_path)
-
+    Environment.getInstance()
     main()
