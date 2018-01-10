a0b138d266a13df0daed53acd73e48e6657ec69a
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2018-01-10 13:53:35 +0100

    override config option only if the regex matches exactly

--- common/src/gosa/common/config.py
+++ common/src/gosa/common/config.py
@@ -119,8 +119,8 @@ class Config(object):
 
         override_options = {}
         for arg in argv:
-            m = re.match("^-{1,2}([^\.]+)\.([^\.]+)=(.*)$", arg)
-            if m:
+            m = re.match("^-{2}([^\.]+)\.([^\.]+)=(.*)$", arg)
+            if m and m.groups() is not None and len(m.groups()) == 3:
                 # section
                 if m.group(1) not in override_options:
                     override_options[m.group(1)] = {}
