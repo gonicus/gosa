07a71b817648b45e5ce36a89b68f84c0738abb71
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2018-02-06 14:47:08 +0100

    Revert "force uid to be lowercase"
    This reverts commit 939cfeb6b6dd754fc6925085c8beb51971b326b2.

--- backend/setup.py
+++ backend/setup.py
@@ -381,8 +381,6 @@ setup(
         UnmarshalFlags = gosa.backend.objects.filter.flags:UnmarshalFlags
         FilterOwnDn = gosa.backend.objects.filter.basic:FilterOwnDn
         AddOwnDnIfEmpty = gosa.backend.objects.filter.basic:AddOwnDnIfEmpty
-        LowerCase = gosa.backend.objects.filter.strings:LowerCase
-        UpperCase = gosa.backend.objects.filter.strings:UpperCase
 
         [gosa.object.comparator]
         like = gosa.backend.objects.comparator.strings:Like
