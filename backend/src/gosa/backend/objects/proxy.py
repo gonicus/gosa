ac9c6f287e5ddd63f178eb340b85fafe4503a401
Tobias Bräutigam <tobias.braeutigam@gonicus.de>
2018-02-19 15:55:46 +0100

    throw no exception when deleting not existant object

--- backend/src/gosa/backend/objects/proxy.py
+++ backend/src/gosa/backend/objects/proxy.py
@@ -164,7 +164,7 @@ class ObjectProxy(object):
             extensions = []
 
         if not base:
-            raise ProxyException(C.make_error('OBJECT_NOT_FOUND', id=dn_or_base))
+            raise ProxyException(C.make_error('OBJECT_NOT_FOUND', id=dn_or_base), status=404)
 
         if self.__read_only is True and base_mode == "create":
             raise ProxyException(C.make_error('CANNOT_CREATE_OBJECT_READ_ONLY', id=dn_or_base))
